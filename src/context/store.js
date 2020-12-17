71ab531c865fafc83cbc0e6279bea48704cb5052
Christian <cjlee300@gmail.com>
2020-12-17 00:23:31 -0500

    Fix for environment variables

--- src/context/store.js
+++ src/context/store.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Client from 'shopify-buy'
 
 const client = Client.buildClient({
-  storefrontAccessToken: process.env.SHOPIFY_ACCESS_TOKEN,
-  domain: `${process.env.SHOP_NAME}.myshopify.com`,
+  storefrontAccessToken: process.env.GATSBY_SHOPIFY_ACCESS_TOKEN,
+  domain: `${process.env.GATSBY_SHOP_NAME}.myshopify.com`,
 })
 
 export const defaultStoreContext = {
